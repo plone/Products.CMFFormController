d8f5bafceff5df5dfd6ad5ace83e71b3e28f150d
Rob Miller <ra@burningman.com>
2006-05-13 08:07:31 +0000

    * only export ZODB controller scripts and validators, not FS versions
    svn path=/CMFFormController/trunk/; revision=23393

--- exportimport/controllerpythonscript.py
+++ exportimport/controllerpythonscript.py
@@ -11,3 +11,12 @@ class ControllerPythonScriptBodyAdapter(PythonScriptBodyAdapter):
     __used_for__ = IControllerPythonScript
 
     suffix = '.cpy'
+
+    def _exportBody(self):
+        """Export the object as a file body.  Don't export FS versions.
+        """
+        if self.context.meta_type == 'Controller Python Script':
+            return PythonScriptBodyAdapter._exportBody(self)
+        return None
+
+    body = property(_exportBody, PythonScriptBodyAdapter._importBody)
--- exportimport/controllervalidator.py
+++ exportimport/controllervalidator.py
@@ -11,3 +11,12 @@ class ControllerValidatorBodyAdapter(PythonScriptBodyAdapter):
     __used_for__ = IControllerValidator
 
     suffix = '.vpy'
+
+    def _exportBody(self):
+        """Export the object as a file body.  Don't export FS versions.
+        """
+        if self.context.meta_type == 'Controller Validator':
+            return PythonScriptBodyAdapter._exportBody(self)
+        return None
+
+    body = property(_exportBody, PythonScriptBodyAdapter._importBody)
