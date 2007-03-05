b6f807b3c0197e0b54ea4c9c9549603ffb216f07
Hanno Schlichting <hanno@hannosch.eu>
2007-03-05 15:59:52 +0000

    Added documentation and missing files to make it a package
    svn path=/CMFFormController/trunk/; revision=38668

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '2.1b1'
+
+setup(name='Products.CMFFormController',
+      version=version,
+      description="CMFFormController provides a form validation mechanism for CMF.",
+      long_description="""\
+      """,
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Zope CMF Plone form validation',
+      author='Geoff Davis',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/CMFFormController/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://plone.org/products/cmfformcontroller/releases',
+      install_requires=[
+        'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
